57982836df77f2d2e6be2961fb553937b7e286ed
kimaya <kimaya@DESKTOP-VGFVTBH>
2023-06-07 11:40:03 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int Number = 10;
+            int Number1 = 20;
+
+            Console.WriteLine("Addtion Is --- "+ Addtion(Number,Number1));
+            Console.ReadLine();
+        }
+
+        public static int Addtion(int A,int B)
+        {
+            return A+ B;
+        }
+    }
+}
